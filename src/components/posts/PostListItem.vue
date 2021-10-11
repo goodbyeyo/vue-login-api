0ea8946de102c4350185035e38e012043bca2383
yoonseol <yoonseol1983@gmail.com>
2021-10-11 23:19:38 +0900

    router navigation guard setting

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,36 +7,35 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
-      {{ postItem.createdAt | formatData }}
+      {{ postItem.createdAt | formatDate }}
       <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
+    <toast-popup></toast-popup>
   </li>
 </template>
 
 <script>
 import { deletePost } from '@/api/posts';
+import ToastPopup from '../common/ToastPopup.vue';
 
 export default {
+  components: { ToastPopup },
   props: {
     postItem: {
       type: Object,
       required: true,
     },
   },
-  // 지역필터
-  // filters: {
-  //   formatData(value) {
-  //     return new Date(value);
-  //   },
-  // },
   methods: {
     async deleteItem() {
-      if (confirm('You want to delete it?')) {
-        await deletePost(this.postItem._id);
-        this.$emit('refresh');
-      }
-      // console.log('deleted');
+      this.commit('showToast', '삭제하시겠습니까?');
+      await deletePost(this.postItem._id);
+      this.$emit('refresh');
+      // if (confirm('You want to delete it?')) {
+      //   await deletePost(this.postItem._id);
+      //   this.$emit('refresh');
+      // }
     },
     routeEditPage() {
       const id = this.postItem._id;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import store from '@/store/index';
 // import LoginPage from '@/views/LoginPage.vue';
 // import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
-  mode: 'history',
+// export default new VueRouter({
+// navigation guard 설정을 위한 변경
+const router = new VueRouter({
+  mode: 'history', // 해시값 관련된 설정
   routes: [
     {
       path: '/',
@@ -23,14 +26,17 @@ export default new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '*',
@@ -38,3 +44,15 @@ export default new VueRouter({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.auth && !store.getters.isLogin) {
+    alert('인증이 필요합니다');
+    // console.log('인증이 필요합니다');
+    next('/login');
+    return;
+  }
+  console.log(to);
+  next();
+});
+export default router;
