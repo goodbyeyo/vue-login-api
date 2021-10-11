72cb3b2c75f4623fcf3ac723a86d6b8fc84ca9b7
yoonseol <yoonseol1983@gmail.com>
2021-10-11 18:02:32 +0900

    date value filer add and grobal setting in main.js

--- src/components/posts/PostListItem.vue
+++ src/components/posts/PostListItem.vue
@@ -7,8 +7,8 @@
       {{ postItem.contents }}
     </div>
     <div class="post-time">
-      {{ postItem.createdAt }}
-      <i class="icon ion-md-create"></i>
+      {{ postItem.createdAt | formatData }}
+      <i class="icon ion-md-create" @click="routeEditPage"></i>
       <i class="icon ion-md-trash" @click="deleteItem"></i>
     </div>
   </li>
@@ -16,6 +16,7 @@
 
 <script>
 import { deletePost } from '@/api/posts';
+
 export default {
   props: {
     postItem: {
@@ -23,13 +24,23 @@ export default {
       required: true,
     },
   },
+  // 지역필터
+  // filters: {
+  //   formatData(value) {
+  //     return new Date(value);
+  //   },
+  // },
   methods: {
     async deleteItem() {
-      if (confirm('you want to delete it?')) {
+      if (confirm('You want to delete it?')) {
         await deletePost(this.postItem._id);
         this.$emit('refresh');
-        console.log('deleted');
       }
+      // console.log('deleted');
+    },
+    routeEditPage() {
+      const id = this.postItem._id;
+      this.$router.push(`/post/${id}`);
     },
   },
 };
